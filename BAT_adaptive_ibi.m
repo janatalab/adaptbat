1471279f18f427c498c86ad4dcd8fbbfc7904c84
Brian Hurley <bkhurley@ucdavis.edu>
2016-09-26 19:26:30 +0000

    BAT updates
    git-svn-id: https://atonal.ucdavis.edu/repos/private/matlab/projects/attmap/bat@3668 28c61866-8173-4a13-8a39-e4c4ff750e15

--- BAT_adaptive_ibi.m
+++ BAT_adaptive_ibi.m
@@ -1,12 +1,13 @@
 function[] = BAT_adaptive_ibi(subject)
+%Ex: BAT_adaptive_ibi('JR1')
 
 % Adaptive inter-beat interval version of the Beat Alignment Test, version 2
 % Written by Jessica Ross, March 2016
-%
+
 % Iversen, JR, Patel, AD. The Beat Alignment Test (BAT): Surveying beat processing 
 % abilities in the general population. Proceedings of the 10th International Conference
 % on Music Perception and Cognition 2008: 465-468. 
-%
+
 % Discriminate between correct ibi and augmented ibi. The 12 stimuli from
 % the BAT are used. Trial 1 and 2 have correct ibi's. Trial 3 has an ibi that
 % has been augmented by 10%. For each trial after trial 3, an incorrect
@@ -16,21 +17,12 @@ function[] = BAT_adaptive_ibi(subject)
 % response will result in the next trial using the same ibi as the previous
 % trial, but with a new musical stimulus. Lengthened and shortened ibi's
 % are alternated.
-%
+
 %OUTPUT FILE:
 %Column 1 = subject's response (off beat = 0, on beat = 1)
 %Column 2 = augmentation condition (off beat = 0, on beat = 1)
 %Column 3 = augmentation amount (0%, -10%, 10%, etc...)
 %Column 4 = correct/incorrect (incorrect response = 1, correct response = 2)
-%
-% INPUT:
-%     subject - subject ID string (e.g., 'JR1')
-%
-% OUTPUT FILE:
-%     Column 1 = subject's response (off beat = 0, on beat = 1)
-%     Column 2 = augmentation condition (off beat = 0, on beat = 1)
-%     Column 3 = augmentation amount (0%, -10%, 10%, etc...)
-%     Column 4 = correct/incorrect (incorrect response = 1, correct response = 2)
 
 Screen('Preference', 'VisualDebugLevel', 1);
 Screen('Preference', 'SkipSyncTests', 1);
@@ -70,16 +62,11 @@ topPriorityLevel = MaxPriority(window);
 Screen('TextSize',window, 30);
 Screen('TextFont',window,'Ariel');
 Screen('TextStyle', window, 1);
-textString = ['Welcome to the experiment. \n\n\n\n\n Please listen to each' ...
-    'musical excerpt and judge whether the \n click track is on beat or off ' ... 
-    'beat. \n\n Respond at the end of each musical excerpt by \n pressing ' ...
-    'q if the tone is off beat \n or p if the tone is on beat. \n\n This ' ...
-    'should take about 7 minutes.'];
+textString = 'Welcome to the experiment. \n\n\n\n\n Please listen to each musical excerpt and judge whether the \n click track is on beat or off beat. \n\n Respond at the end of each musical excerpt by \n pressing q if the tone is off beat \n or p if the tone is on beat. \n\n This should take about 7 minutes.';
 DrawFormattedText(window, textString, 'center', 'center', white);
 
 Screen('TextSize',window, 15);
-textString = ['\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'...
-    '< Press any key to continue >'];
+textString = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n< Press any key to continue >';
 DrawFormattedText(window, textString, 'center', 'center', white);
 
 Screen('Flip', window);
@@ -150,12 +137,9 @@ offp9 = dir([data_loc,'*_B9_v2.wav']);dirlist_offp9 = {offp9.name};
 offp10 = dir([data_loc,'*_B10_v2.wav']);dirlist_offp10 = {offp10.name};
 
 wavfilename = [];
-aug_intervals = [on0 offn1 offn2 offn3 offn4 offn5 offn6 offn7 offn8 offn9 ...
-    offn10 offp1 offp2 offp3 offp4 offp5 offp6 offp7 offp8 offp9 offp10];
-aug_cols = [1,21,11,20,10,19,9,18,8,1,1,17,17,7,7,16,16,6,6,15,15,1,1,5,5, ...
-    14,14,4,4,1,1,13,13,3,3,12,12,2,2,1,1,1,1,1,1,1,1,1,1,1,1];
-aug_amount = [0 10 -10 9 -9 8 -8 7 -7 0 0 6 6 -6 -6 5 5 -5 -5 4 4 0 0 -4 -4 ... 
-    3 3 -3 -3 0 0 2 2 -2 -2 1 1 -1 -1 0 0 0 0 0 0 0 0 0 0 0 0];
+aug_intervals = [on0 offn1 offn2 offn3 offn4 offn5 offn6 offn7 offn8 offn9 offn10 offp1 offp2 offp3 offp4 offp5 offp6 offp7 offp8 offp9 offp10];
+aug_cols = [1,21,11,20,10,19,9,18,8,1,1,17,17,7,7,16,16,6,6,15,15,1,1,5,5,14,14,4,4,1,1,13,13,3,3,12,12,2,2,1,1,1,1,1,1,1,1,1,1,1,1];
+aug_amount = [0 10 -10 9 -9 8 -8 7 -7 0 0 6 6 -6 -6 5 5 -5 -5 4 4 0 0 -4 -4 3 3 -3 -3 0 0 2 2 -2 -2 1 1 -1 -1 0 0 0 0 0 0 0 0 0 0 0 0];
 
 trial_order = {};
 k = randperm(12);
