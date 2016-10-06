6321e9f121800386f6a7b5426d49f6755d3e341d
Janata Lab <janatalab@gmail.com>
2016-10-06 23:35:45 +0000

    git-svn-id: https://atonal.ucdavis.edu/repos/private/matlab/projects/attmap/bat@3678 28c61866-8173-4a13-8a39-e4c4ff750e15

---
+++ bat_initialize_ptb.m
@@ -0,0 +1,91 @@
+function ptb = bat_initialize_ptb(params)
+
+% Itializes PsychToolbox graphics and audio for BAT experiment. Stores
+% graphics and audio handles in structure array called ptb.
+% 
+% INPUT:
+%     params -- parameter structure array with the following fields:
+%          * freq - frequency at which audio should be played
+%          * nrchannels - 1 (mono) or 2 (stereo) playback
+% OUTPUT:
+%     ptb -- structure array with PsychToolbox object handles that will be
+%     needed in the experiment function
+%
+% 6 OCT 2016 - Brian K. Hurley
+
+%% Graphics Initialization
+
+Screen('Preference', 'VisualDebugLevel', 1);
+Screen('Preference', 'SkipSyncTests', 1);
+
+%HideCursor;
+PsychDefaultSetup(2)
+
+%Set screen number to my one screen (this can detect multiple displays if
+%needed)
+screenNum = max(Screen('Screens'));
+
+%Define colors
+ptb.white = WhiteIndex(screenNum);
+black = BlackIndex(screenNum);
+
+%% Instruction Screen
+
+%Black screen appears first
+[ptb.window, ~] = PsychImaging('OpenWindow', screenNum, black);
+Screen('Flip',ptb.window);
+
+% Deliver instruction text
+Screen('TextSize',ptb.window, 30);
+Screen('TextFont',ptb.window,'Ariel');
+Screen('TextStyle', ptb.window, 1);
+textString = ['Please listen to each musical excerpt and judge whether ' ...
+  'the click track is on-beat or off-beat. \n\n\n Respond at the end of ' ... 
+  'each musical excerpt by pressing Q if the tone is off-beat or P if ' ... 
+  'the tone is on-beat.'];
+DrawFormattedText(ptb.window, textString, 'center', 'center', ptb.white, 60);
+
+Screen('TextSize',ptb.window, 15);
+textString = '< Press any key to continue >';
+DrawFormattedText(ptb.window, textString, 'center', 1000, ptb.white);
+
+Screen('Flip', ptb.window);
+KbStrokeWait;
+
+% subject will see abbreviated on-screen instructions throughout task
+Screen('TextSize',ptb.window, 30);
+textString = 'When music ends\n\n\nPress Q for off-beat, P for on-beat';
+DrawFormattedText(ptb.window, textString, 'center', 'center', ptb.white);
+Screen('Flip', ptb.window);
+WaitSecs(2)
+
+%% Audio Initialization
+
+% Make sure we start with PsychPortAudio closed so that soundcard is not
+% blocked
+try 
+  PsychPortAudio('Close');
+end
+
+% Initialize Sounddriver
+InitializePsychSound(1);
+
+% Open Psych-Audio port, with the follow arguements
+% (1) [] = default sound device
+% (2) 1 = sound playback only
+% (3) 1 = default level of latency
+% (4) Requested frequency in samples per second
+% (5) 2 = stereo putput
+ptb.pahandle = PsychPortAudio('Open', [], 1, 1, params.freq, params.nrchannels);
+
+% Keyboard Information
+ptb.escapeKey = KbName('ESCAPE');
+ptb.NO = KbName('q'); % Q key = NO
+ptb.YES = KbName('p'); % P key = YES
+
+% Start immediately (0 = immediately)
+ptb.startCue = 0;
+
+% Should we wait for the device to really start (1 = yes)
+% INFO: See help PsychPortAudio
+ptb.waitForDeviceStart = 1;
